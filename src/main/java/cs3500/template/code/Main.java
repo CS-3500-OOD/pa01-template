3410ca5cb5f25a6f212c6523efd4924d2109994f
Mark Fontenot <mfonten@lyle.smu.edu>
2023-05-09 13:53:29 -0400

    Template Repo for PA 01 push

---
+++ src/main/java/cs3500/template/code/Main.java
@@ -0,0 +1,16 @@
+package cs3500.template.code;
+
+
+/**
+ * This is the main driver of this project.
+ */
+public class Main {
+  /**
+   * Project entry point
+   *
+   * @param args - no command line args required
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello from PA01 Template Repo");
+  }
+}
